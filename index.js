ddc016ddd4658e1fd5f7ab31c86bac13de099270
Tejesh Reddy <64296310+tejesh856@users.noreply.github.com>
2024-02-10 18:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 mongodb().then(async () => {
     await app.use(bodyParser.urlencoded({ extended: true }));
     await app.use(bodyParser.json());
-    await app.use(express.static('public'));
+    await app.use('/',express.static('public'));
     await app.use('/',require('./routes/routes'));
     await app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
